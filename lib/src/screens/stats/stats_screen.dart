d0d3d2b869dccf2284ea9f210b02d26a9410bf1e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-22 04:07:33 +0700

    stats

--- lib/src/screens/stats/stats_screen.dart
+++ lib/src/screens/stats/stats_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_todos_firebase/src/application/stats/stats_bloc.dart';
 
 class StatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('stats'),
-    );
+    return BlocBuilder<StatsBloc, StatsState>(builder: (context, state) {
+      if (state is StatsLoadInProgress) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (state is StatsLoadSuccess) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Completed Todos',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(height: 8),
+              Text(
+                '${state.numCompleted}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Active Todos',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(height: 8),
+              Text(
+                '${state.numActive}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ],
+          ),
+        );
+      } else {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    });
   }
 }
