773157ed462ae158817174c6e1cf8edeea5cc515
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-06-02 16:29:21 +0700

    u

--- lib/src/domain/repository/todos_repository.dart
+++ lib/src/domain/repository/todos_repository.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter_bloc_todos_firebase/src/data/model/todo_model.dart';
 
 abstract class TodosRepository {
+  Stream<List<TodoModel>> todos();
   Future<void> addNewTodo(TodoModel todo);
   Future<void> deleteTodo(TodoModel todo);
-  Stream<List<TodoModel>> todos();
   Future<void> updateTodo(TodoModel todo);
 }
